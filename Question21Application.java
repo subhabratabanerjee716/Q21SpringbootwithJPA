f2308352e8d1c4af1184b8e8338f6a6bffe0d179
Subhabrata Banerjee <63787106+subhabratabanerjee716@users.noreply.github.com>
2023-07-24 13:58:44 +0530

    Create Question21Application.java

---
+++ Question21Application.java
@@ -0,0 +1,13 @@
+package ineuron.assessment.Q21.question21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Question21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Question21Application.class, args);
+	}
+
+}
